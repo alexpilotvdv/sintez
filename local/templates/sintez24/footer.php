2e45f78fbfa858ec1d81c4a3a96a788e9a8b43ce
alexpilot <a41751@gmail.com>
2020-09-02 15:57:12 +0300

    first

---
+++ local/templates/sintez24/footer.php
@@ -0,0 +1,10 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+</div>
+</div>
+</div>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
